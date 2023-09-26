065805e098beb7bd3143c81107f5a80560cb7b28
Roma Lomachinskiy <54740014+RomaLoma91@users.noreply.github.com>
2023-09-26 10:15:15 +0300

    Update models.py

--- src/database/models.py
+++ src/database/models.py
@@ -16,26 +16,12 @@ class Contact(Base):
     first_name = Column(String(55), nullable=False)
     last_name = Column(String(55), nullable=False)
     birthday = Column("birthday", Date)
-
-    created_at = Column(DateTime, default=func.now())
-    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
-
-    emails = relationship("Email", backref="contacts")
-    phones = relationship("Phone", backref="contacts")
-
-
-class Email(Base):
-    __tablename__ = "emails"
-
-    id = Column(Integer, primary_key=True)
     email = Column(String(50), nullable=False, unique=True)
-    contact_id = Column(Integer, ForeignKey("contacts.id"), nullable=False)
 
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
 
-    contact = relationship("Contact", back_populates="emails")
-
+    phones = relationship("Phone", backref="contacts")
 
 class Phone(Base):
     __tablename__ = "phones"
